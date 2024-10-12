6529426db671119b13c28ca2840f706673bd9556
AlexCitrus <kidong044@gmail.com>
2024-10-13 01:46:51 +0800

    [FIX] Remove unused styles

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { LogBox } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./store";
@@ -41,12 +39,3 @@ export default function App() {
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
